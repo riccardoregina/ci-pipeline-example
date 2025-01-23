1abfe986b9b50a10cec68086bb7559b813b02281
riccardoregina <riccardoregina04@gmail.com>
2025-01-23 12:04:43 +0100

    very first commit

---
+++ src/main/java/it/unina/ci_pipeline/CiPipelineApplication.java
@@ -0,0 +1,13 @@
+package it.unina.ci_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unina/ci_pipeline/CiPipelineApplicationTests.java
@@ -0,0 +1,19 @@
+package it.unina.ci_pipeline;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void ok() {
+		Assertions.assertTrue(true);
+	}
+
+}
